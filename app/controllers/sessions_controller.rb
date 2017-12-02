8b1f20be52d9b3f0c8093d719d74cbf97c5fc300
DanielYouCan <danbizarre@gmail.com>
2017-12-02 19:45:44 +0700

    User redirection to requested path using cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
 
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to tests_path
+      redirect_to cookies[:current_url]
     else
       flash.now[:alert] = 'Wrong input! Check it for mistakes'
       render :new
